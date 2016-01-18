988b5ef2a522754d77807b58d14a029c7a8d5fc5
sriramoman <sriramoman@gmail.com>
2016-01-17 19:40:52 -0700

    Partial output verified

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,15 +1,43 @@
 //
-//  main.cpp
-//  LinkedList
-//
 //  Created by Sriram Vellangallor Subramanian on 1/17/16.
 //  Copyright © 2016 Sriram Vellangallor Subramanian. All rights reserved.
 //
-
+//  https://www.hackerrank.com/challenges/maxsubarray
+//
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t,n;
+    cin>>t;
+    for (int i=0; i<t; i++) {
+        cin>>n;
+        int j;
+        vector<int> arrNumbers;
+        for (j=0; j<n; j++) {
+            int elem;
+            cin>>elem;
+            arrNumbers.push_back(elem);
+        }
+        int maxsum=arrNumbers[0];
+        int noncontsum=0;
+        for (j=0; j<n; j++) {
+            if(arrNumbers[j]>-1)
+                noncontsum+=arrNumbers[j];
+            int sum=arrNumbers[j];
+            maxsum=sum>maxsum?sum:maxsum;
+            for (int k=j+1; k<n; k++) {
+                sum+=arrNumbers[k];
+                maxsum=sum>maxsum?sum:maxsum;
+            }
+        }
+        cout<<maxsum<<" "<<noncontsum<<endl;
+    }
     return 0;
 }
